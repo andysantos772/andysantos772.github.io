8085367e1e1bb1d458d9a37763b7b0fd69e8da64
unknown <andysantos436@gmail.com>
2025-01-03 18:13:41 -0800

    Add simple nav.js DOM elements

---
+++ nav.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('nav');
+
+nav.innerHTML = `
+<ul>
+	<li><a href="">Home</a></li>
+	<li><a href="">About</a></li>
+	<li><a href="">Resume</a></li>
+	<li><a href="">Projects</a></li>
+	<li><a href="">Contact</a></li>
+</ul>
+`
